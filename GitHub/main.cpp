6a97a55da462eb3fbc2d3eaa4c1ce87020395666
Qi Zhang <jacky780203@sina.com>
2021-10-24 21:05:23 -0400

    Initial Commit

---
+++ GitHub/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitHub
+//
+//  Created by Qi Zhang on 10/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
